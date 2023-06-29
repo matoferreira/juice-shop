7ccf8ed522fb6e51e9069793c09e922bba88e60b
Matias <matifer1@gmail.com>
2023-06-28 23:45:17 -0300

    all vulns, no changes

--- routes/login.ts
+++ routes/login.ts
@@ -33,18 +33,7 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-  
-    models.sequelize.query(
-      'SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL',
-      {
-        model: UserModel,
-        plain: true,
-        replacements: {
-          email: req.body.email  '',
-          password: security.hash(req.body.password  '')
-        }
-      }
-    )
+    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
@@ -62,8 +51,7 @@ module.exports = function login () {
         } else {
           res.status(401).send(res.__('Invalid email or password.'))
         }
-      })
-      .catch((error: Error) => {
+      }).catch((error: Error) => {
         next(error)
       })
   }
