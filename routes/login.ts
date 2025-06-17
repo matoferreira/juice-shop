6c6cdaa2dfd56678a1e64b87421d7e9d3577f803
matoferreira <MATIAS.FERREIRA@correo.ucu.edu.uy>
2025-06-17 09:36:10 -0300

    commitespanol

--- routes/login.ts
+++ routes/login.ts
@@ -33,7 +33,16 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+        models.sequelize.query(
+      'SELECT * FROM Users WHERE email = :email  AND password = :password AND detedAt IS NULL',
+    {
+      model: UserModel,
+      plain: true,
+      replacements: {
+        email: req.body.email || '',
+        password: security.hash(req.body.password || '')
+      }
+    })
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
@@ -47,7 +56,7 @@ module.exports = function login () {
             }
           })
         } else if (user.data?.id) {
-          afterLogin(user, res, next)
+          afterLogin(user, res, next);
         } else {
           res.status(401).send(res.__('Invalid email or password.'))
         }
